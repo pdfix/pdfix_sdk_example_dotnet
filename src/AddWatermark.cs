5c0d9c8c3f8bb2c28d63bda13f8d8c47541d758f
Szabi Szamel <szabi.szamel@pdfix.net>
2021-05-11 13:52:09 +0200

    SDK-572 Updated addwatermark sample

--- src/AddWatermark.cs
+++ src/AddWatermark.cs
@@ -5,7 +5,6 @@
 
 using System;
 using System.IO;
-using System.Xml.Linq;
 using PDFixSDK.Pdfix;
 
 namespace PDFix.App.Module
@@ -40,17 +39,32 @@ namespace PDFix.App.Module
             if (image_obj == null)
                 throw new Exception(pdfix.GetError());
 
-            // set watermark params
-            watermarkParams.page_range.start_page = 1;
-            watermarkParams.page_range.end_page = 3;
-            watermarkParams.page_range.page_range_spec = PdfPageRangeType.kEvenPagesOnly;
-            watermarkParams.h_value = 10;
-            watermarkParams.v_value = 10;
-            watermarkParams.scale = (float)0.5;
-            watermarkParams.opacity = (float)0.5;
-
-            if (!doc.AddWatermarkFromImage(watermarkParams, image_obj))
+            var page = doc.AcquirePage(0);
+            if (page == null)
                 throw new Exception(pdfix.GetError());
+
+            var content = page.GetContent();
+            if (content == null)
+                throw new Exception(pdfix.GetError());
+
+            var xobjdict = image_obj.GetStreamDict();
+            var scale = 0.5f;
+            var width = xobjdict.GetNumber("Width") * scale;
+            var height = xobjdict.GetNumber("Height") * scale;
+            var h_value = 20.0f;
+            var v_value = 50.0f;
+
+            //z order represented by the position in the content, -1 or last add to the top, 0 to the background
+            var imageobject = content.AddNewImage(-1, image_obj, new PdfMatrix(width, 0, 0, height, h_value, v_value));
+
+            //set opacity of the image 0-255
+            var graphicState = imageobject.GetGState();
+            graphicState.color_state.fill_opacity = 200;
+            imageobject.SetGState(graphicState);
+
+            page.SetContent();
+
+            page.Release();
 
             if (!doc.Save(savePath, Pdfix.kSaveFull))
                 throw new Exception(pdfix.GetError());
